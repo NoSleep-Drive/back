cbc32ba77cbd9b9416fff1a0656169c3b572c58b
kimhji <twin4185@naver.com>
2025-04-10 14:31:52 +0900

    chore: Initial commit

---
+++ src/main/java/com/nosleepdrive/nosleepdrivebackend/NosleepdriveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nosleepdrive.nosleepdrivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosleepdriveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosleepdriveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nosleepdrive/nosleepdrivebackend/NosleepdriveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosleepdrive.nosleepdrivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosleepdriveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

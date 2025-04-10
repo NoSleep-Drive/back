deab9f589cf7dcb1db99df04001c7ce2eed40725
kimhji <twin4185@naver.com>
2025-04-11 01:44:23 +0900

    chore[#1]: commit 확인 에러 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-    parserPreset: {
-        parserOpts: {
-            headerPattern: /^(\w+)\[#(\d+)\]:\s(.+)$/,
-            headerCorrespondence: ['type', 'ticket', 'subject'],
-        },
-    },
     rules: {
-        'header-match-pattern': [2, 'always', /^(\w+)\[#(\d+)\]:\s(.+)$/],
         'type-enum': [
             2,
             'always',
             ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'style', 'ci', 'perf'],
         ],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'subject-empty': [2, 'never'],
+        'subject-case': [0], // 자유롭게
+        'header-pattern': [/^(\w+)\[#\d+\]:\s.+$/],
+        'header-pattern-required': [2, 'always'],
     },
 };

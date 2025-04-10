2cfe362ac68e59df7e579ed22e9d6903c710555f
kimhji <twin4185@naver.com>
2025-04-11 01:49:27 +0900

    chore[#1]: commit 확인 에러 수정2

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,8 +8,6 @@ module.exports = {
         'type-case': [2, 'always', 'lower-case'],
         'type-empty': [2, 'never'],
         'subject-empty': [2, 'never'],
-        'subject-case': [0], // 자유롭게
-        'header-pattern': [/^(\w+)\[#\d+\]:\s.+$/],
-        'header-pattern-required': [2, 'always'],
+        'header-pattern': [2, 'always', /^(\w+)\[#\d+\]:\s.+$/],
     },
 };

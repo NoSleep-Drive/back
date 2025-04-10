d49a9775383a0145ccb043f5440778aee18c2c18
kimhji <twin4185@naver.com>
2025-04-11 02:07:20 +0900

    chore[#1]: CI 과정에서 DB 연결 과정 생략

--- src/test/java/com/nosleepdrive/nosleepdrivebackend/NosleepdriveBackendApplicationTests.java
+++ src/test/java/com/nosleepdrive/nosleepdrivebackend/NosleepdriveBackendApplicationTests.java
@@ -1,9 +1,11 @@
 package com.nosleepdrive.nosleepdrivebackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class NosleepdriveBackendApplicationTests {
 
     @Test

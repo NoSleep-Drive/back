3532d0a125c3e8318e347e54027ac1eaf57f82ce
kimhji <twin4185@naver.com>
2025-04-20 19:37:30 +0900

    chore[#3]: remove duplicating entity scan

--- src/main/java/com/nosleepdrive/nosleepdrivebackend/NosleepdriveBackendApplication.java
+++ src/main/java/com/nosleepdrive/nosleepdrivebackend/NosleepdriveBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.nosleepdrive.nosleepdrivebackend")
 public class NosleepdriveBackendApplication {
 
     public static void main(String[] args) {

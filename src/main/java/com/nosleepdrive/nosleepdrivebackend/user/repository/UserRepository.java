85581bfbaa2cfb21cba98d1508e668ff802472c2
kimhji <twin4185@naver.com>
2025-04-26 23:39:10 +0900

    feat[#5]: remove unnecessary user Entity

--- src/main/java/com/nosleepdrive/nosleepdrivebackend/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nosleepdrive.nosleepdrivebackend.user.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.nosleepdrive.nosleepdrivebackend.user.repository.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}

0a9b74e028202cf728e61de510a11a5b8015764c
kimhji <twin4185@naver.com>
2025-04-26 23:54:18 +0900

    feat[#5]: add Entity driver

---
+++ src/main/java/com/nosleepdrive/nosleepdrivebackend/driver/repository/entity/Driver.java
@@ -0,0 +1,27 @@
+package com.nosleepdrive.nosleepdrivebackend.driver.repository.entity;
+
+import com.nosleepdrive.nosleepdrivebackend.vehicle.repository.entity.Vehicle;
+import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
+
+import java.sql.Date;
+
+@Entity
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_driver", updatable = false)
+    private Long idDriver;
+
+    @NonNull
+    @Column(name = "start_time")
+    private Date startTime;
+
+    @Column(name = "end_time")
+    private Date endTime;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_vehicle", referencedColumnName = "id_vehicle")
+    Vehicle vehicle;
+}
